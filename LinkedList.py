0fe742c3040001a798c9a89e08a367130738e30b
anthonytkwu <anthonytkwu@gmail.com>
2022-12-25 21:58:33 -0800

    First Commit

---
+++ LinkedList.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+class SLinkedList:
+    def __init__(self, node=None):
+        self.headval = node
+
+    def listprint(self):
+        printval = self.headval
+        while printval is not None:
+            print (printval.dataval)
+            printval = printval.nextval
+
+    def addNodeEnd(self, node):
+        if self.headval == None:
+            self.headval = node
+            return
+        tester = self.headval
+        while tester.nextval != None:
+            tester = tester.nextval
+        tester.nextval = node
+    
+
+
+list = SLinkedList(Node("Mon"))
+e1 = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+
+list.addNodeEnd(e2)
+list.addNodeEnd(e3)
+
+#list.remove("Mon")
+
+list.listprint()
